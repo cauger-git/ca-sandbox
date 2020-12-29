25b985099ef1ea87426f1c25106cc4a857d7f48d
Charles Auger <charles.auger@canada.ca>
2020-12-29 13:09:01 -0500

    Initial check-in

---
+++ api/db_connect/app.py
@@ -0,0 +1,32 @@
+import os
+import json
+import simplejson
+from flask import Flask
+from flask.json import jsonify
+import psycopg2
+from sqlalchemy import create_engine, MetaData, Table
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.sql.schema import Column
+from sqlalchemy.types import String, Numeric
+
+app = Flask(__name__)
+
+def login_db():
+    global TABLE_META
+    db_user = os.environ['DB_USER']
+    db_pass = os.environ['DB_PASS']
+    db_string = "oracle://" + db_user + ":" + db_pass + "@stsorau.IC.GC.CA:1521/uat01ls"
+    db_engine = create_engine(db_string)
+    db_session = sessionmaker(bind=db_engine)
+    #TABLE_META = MetaData(db_engine, schema="mini_spectra")
+    return db_session()
+
+@app.route('/')
+def index():
+    
+    # Login to database
+    db_session = login_db()
+
+    return 'Connected to DB'
+
+app.run(host='0.0.0.0', port=8080)
